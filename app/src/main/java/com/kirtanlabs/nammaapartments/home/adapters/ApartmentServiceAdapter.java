2c54cada597b5749d22d287439a297dbb49dae9d
Ashish Jha <iamashishjha@gmail.com>
2018-08-11 22:06:40 +0530

    Implemented Call, Message, Whatsapp and Refer functionality for Apartment Services

--- app/src/main/java/com/kirtanlabs/nammaapartments/home/adapters/ApartmentServiceAdapter.java
+++ app/src/main/java/com/kirtanlabs/nammaapartments/home/adapters/ApartmentServiceAdapter.java
@@ -1,14 +1,23 @@
 package com.kirtanlabs.nammaapartments.home.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.kirtanlabs.nammaapartments.BaseActivity;
 import com.kirtanlabs.nammaapartments.R;
+import com.kirtanlabs.nammaapartments.utilities.Constants;
+
+import java.net.URLEncoder;
 
 import static com.kirtanlabs.nammaapartments.utilities.Constants.setLatoBoldFont;
 import static com.kirtanlabs.nammaapartments.utilities.Constants.setLatoRegularFont;
@@ -20,9 +29,11 @@ public class ApartmentServiceAdapter extends RecyclerView.Adapter<ApartmentServi
      * ------------------------------------------------------------- */
 
     private final Context mCtx;
+    private final BaseActivity baseActivity;
 
     public ApartmentServiceAdapter(Context mCtx) {
         this.mCtx = mCtx;
+        baseActivity = (BaseActivity) mCtx;
     }
 
     @NonNull
@@ -36,12 +47,11 @@ public class ApartmentServiceAdapter extends RecyclerView.Adapter<ApartmentServi
 
     @Override
     public void onBindViewHolder(@NonNull ApartmentServiceAdapter.ApartmentServiceViewHolder holder, int position) {
-
-        //TODO: Hardcoded data will be removed once the actual data is there to retrieve
+        //TODO: Data is hardcoded for now. Will be replaced by actual data once it's available.
         holder.textApartmentServiceNameValue.setText("Ashish Jha");
         holder.textApartmentServiceRatingValue.setText("3");
-        holder.textApartmentServiceTimeSlotValue.setText("8 PM - 10 PM");
-        holder.textApartmentServiceNoOfFlatsSlotValue.setText("4");
+        holder.textApartmentServiceTimeSlotValue.setText("11 AM");
+        holder.textApartmentServiceNoOfFlatsSlotValue.setText("5");
     }
 
     @Override
@@ -49,7 +59,47 @@ public class ApartmentServiceAdapter extends RecyclerView.Adapter<ApartmentServi
         return 3;
     }
 
-    class ApartmentServiceViewHolder extends RecyclerView.ViewHolder {
+    /* ------------------------------------------------------------- *
+     * Private Methods
+     * ------------------------------------------------------------- */
+
+    /**
+     * This method is invoked when user presses on the 'WhatsApp' icon in Card View. User will then be able
+     * to contact the Apartment Service via WhatsApp
+     */
+    private void redirectUserToWhatsApp() {
+        PackageManager pm = mCtx.getPackageManager();
+        Intent whatsappIntent = new Intent(Intent.ACTION_VIEW);
+        String message = mCtx.getString(R.string.whatsapp_msg);
+
+        try {
+            //TODO: Mobile Number has been hardcoded. Will be replaced later.
+            String url = "https://api.whatsapp.com/send?phone="+ "+919885665744" +"&text=" + URLEncoder.encode(message, "UTF-8");
+            whatsappIntent.setPackage("com.whatsapp");
+            whatsappIntent.setData(Uri.parse(url));
+            if (whatsappIntent.resolveActivity(pm) != null) {
+                mCtx.startActivity(whatsappIntent);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method is invoked when user presses on the 'Refer' icon in Card View. User will then be able
+     * to share the Apartment Service with his Contacts
+     */
+    private void redirectUserToShareWith() {
+        Intent referIntent = new Intent(android.content.Intent.ACTION_SEND);
+        referIntent.setType("text/plain");
+        mCtx.startActivity(Intent.createChooser(referIntent, mCtx.getString(R.string.refer_title)));
+    }
+
+    /* ------------------------------------------------------------- *
+     * Apartment Service Holder class
+     * ------------------------------------------------------------- */
+
+    class ApartmentServiceViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         /* ------------------------------------------------------------- *
          * Private Members
@@ -63,6 +113,10 @@ public class ApartmentServiceAdapter extends RecyclerView.Adapter<ApartmentServi
         private final TextView textApartmentServiceTimeSlotValue;
         private final TextView textApartmentServiceNoOfFlats;
         private final TextView textApartmentServiceNoOfFlatsSlotValue;
+        private final TextView textCall;
+        private final TextView textMessage;
+        private final TextView textWhatsapp;
+        private final TextView textRefer;
 
         /* ------------------------------------------------------------- *
          * Constructor
@@ -79,6 +133,10 @@ public class ApartmentServiceAdapter extends RecyclerView.Adapter<ApartmentServi
             textApartmentServiceRatingValue = itemView.findViewById(R.id.textApartmentServiceRatingValue);
             textApartmentServiceTimeSlotValue = itemView.findViewById(R.id.textApartmentServiceTimeSlotValue);
             textApartmentServiceNoOfFlatsSlotValue = itemView.findViewById(R.id.textApartmentServiceFlatsValue);
+            textCall = itemView.findViewById(R.id.textCall);
+            textMessage = itemView.findViewById(R.id.textMessage);
+            textWhatsapp = itemView.findViewById(R.id.textWhatsapp);
+            textRefer = itemView.findViewById(R.id.textRefer);
 
             /*Setting font for all the views*/
             textApartmentServiceName.setTypeface(setLatoBoldFont(mCtx));
@@ -89,6 +147,36 @@ public class ApartmentServiceAdapter extends RecyclerView.Adapter<ApartmentServi
             textApartmentServiceRatingValue.setTypeface(setLatoRegularFont(mCtx));
             textApartmentServiceTimeSlotValue.setTypeface(setLatoRegularFont(mCtx));
             textApartmentServiceNoOfFlatsSlotValue.setTypeface(setLatoRegularFont(mCtx));
+            textCall.setTypeface(Constants.setLatoBoldItalicFont(mCtx));
+            textMessage.setTypeface(Constants.setLatoBoldItalicFont(mCtx));
+            textWhatsapp.setTypeface(Constants.setLatoBoldItalicFont(mCtx));
+            textRefer.setTypeface(Constants.setLatoBoldItalicFont(mCtx));
+
+            /*Setting onClick Listeners*/
+            textCall.setOnClickListener(this);
+            textMessage.setOnClickListener(this);
+            textWhatsapp.setOnClickListener(this);
+            textRefer.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                //TODO: Mobile Number has been hardcoded. Will be replaced later.
+                case R.id.textCall:
+                    baseActivity.makePhoneCall("9986553474");
+                    break;
+                case R.id.textMessage:
+                    baseActivity.sendTextMessage("9986553474", mCtx.getString(R.string.message_body));
+                    break;
+                case R.id.textWhatsapp:
+                    redirectUserToWhatsApp();
+                    break;
+                case R.id.textRefer:
+                    redirectUserToShareWith();
+                    break;
+            }
         }
     }
 }
---
+++ app/src/main/java/com/kirtanlabs/nammaapartments/home/pojo/RetrieveNammaApartmentService.java
@@ -0,0 +1,47 @@
+package com.kirtanlabs.nammaapartments.home.pojo;
+
+public class RetrieveNammaApartmentService {
+
+    /* ------------------------------------------------------------- *
+     * Private Members
+     * ------------------------------------------------------------- */
+
+    private String fullName;
+    private int rating;
+    private String timeSlot;
+    private String noOfFlats;
+    private String phoneNumber;
+
+    /* ------------------------------------------------------------- *
+     * Constructors
+     * ------------------------------------------------------------- */
+
+    public RetrieveNammaApartmentService() {
+    }
+
+    RetrieveNammaApartmentService(String fullName, int rating, String timeSlot, String noOfFlats, String phoneNumber) {
+        this.fullName = fullName;
+        this.rating = rating;
+        this.timeSlot = timeSlot;
+        this.noOfFlats = noOfFlats;
+        this.phoneNumber = phoneNumber;
+    }
+
+    /* ------------------------------------------------------------- *
+     * Getters
+     * ------------------------------------------------------------- */
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getRating() { return rating; }
+
+    public String getTimeSlot() { return timeSlot; }
+
+    public String getNoOfFlats() { return noOfFlats; }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
