0d4b384dfb9d19dd283a69fb550e1429e1a8fa2d
Roshan Halwai <roshan.halwai@gmail.com>
2018-09-19 04:18:50 +0530

    Added storage bucket, sender id and project id for different environment

--- app/src/main/java/com/kirtanlabs/nammaapartments/NammaApartmentsGlobal.java
+++ app/src/main/java/com/kirtanlabs/nammaapartments/NammaApartmentsGlobal.java
@@ -32,12 +32,18 @@ public class NammaApartmentsGlobal extends Application {
                 .setApplicationId("1:896005326129:android:6ff623e10a2664c5")
                 .setApiKey("AIzaSyD3Ar2J0gJ8AiL8s0BVlkWP3PXbux3bvKU")
                 .setDatabaseUrl("https://nammaapartments-beta.firebaseio.com/")
+                .setStorageBucket("nammaapartments-beta.appspot.com")
+                .setGcmSenderId("896005326129")
+                .setProjectId("nammaapartments-beta")
                 .build();
 
         final FirebaseOptions DEV_ENV_OPTIONS = new FirebaseOptions.Builder()
                 .setApplicationId("1:703896080530:android:67ab068074f57ad3")
                 .setApiKey("AIzaSyA-F_DSWIb-HRx1bAE5f5aW1TT4npAME60")
                 .setDatabaseUrl("https://nammaapartments-development.firebaseio.com/")
+                .setStorageBucket("nammaapartments-development.appspot.com")
+                .setGcmSenderId("703896080530")
+                .setProjectId("nammaapartments-development")
                 .build();
 
         FirebaseApp.initializeApp(this, BETA_ENV_OPTIONS, BETA_ENV);
