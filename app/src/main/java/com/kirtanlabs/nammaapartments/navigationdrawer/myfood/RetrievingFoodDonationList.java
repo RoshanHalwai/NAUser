300e6ef6902ca3208dcacae399f6d4e9876f420a
Avinash <chappa.avinash200@gmail.com>
2018-09-20 13:37:26 +0530

    Retrieved the food Donation requests raised by the user and displayed in donate food history screen

---
+++ app/src/main/java/com/kirtanlabs/nammaapartments/navigationdrawer/myfood/RetrievingFoodDonationList.java
@@ -0,0 +1,109 @@
+package com.kirtanlabs.nammaapartments.navigationdrawer.myfood;
+
+import android.content.Context;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.kirtanlabs.nammaapartments.NammaApartmentsGlobal;
+import com.kirtanlabs.nammaapartments.navigationdrawer.myfood.pojo.DonateFood;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.kirtanlabs.nammaapartments.utilities.Constants.DONATE_FOOD_REFERENCE;
+import static com.kirtanlabs.nammaapartments.utilities.Constants.FIREBASE_CHILD_FOOD_DONATIONS;
+
+public class RetrievingFoodDonationList {
+
+    /* ------------------------------------------------------------- *
+     * Private Members
+     * ------------------------------------------------------------- */
+
+    private final Context context;
+    private int count = 0;
+
+    /* ------------------------------------------------------------- *
+     * Constructor
+     * ------------------------------------------------------------- */
+
+    public RetrievingFoodDonationList(Context context) {
+        this.context = context;
+    }
+
+    /* ------------------------------------------------------------- *
+     * Private Methods
+     * ------------------------------------------------------------- */
+
+    private void getFoodDonationUIDList(FoodDonationUIDListCallback foodDonationUIDListCallback) {
+        NammaApartmentsGlobal nammaApartmentsGlobal = ((NammaApartmentsGlobal) context.getApplicationContext());
+        DatabaseReference userDataFoodDonationReference = nammaApartmentsGlobal.getUserDataReference().child(FIREBASE_CHILD_FOOD_DONATIONS);
+        userDataFoodDonationReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    ArrayList<String> notificationUIDList = new ArrayList<>();
+                    for (DataSnapshot notificationUIDDataSnapshot : dataSnapshot.getChildren()) {
+                        notificationUIDList.add(notificationUIDDataSnapshot.getKey());
+                    }
+                    if (dataSnapshot.getChildrenCount() == notificationUIDList.size()) {
+                        foodDonationUIDListCallback.onCallBack(notificationUIDList);
+                    }
+                } else {
+                    foodDonationUIDListCallback.onCallBack(new ArrayList<>());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+    /* ------------------------------------------------------------- *
+     * Public Methods
+     * ------------------------------------------------------------- */
+
+    public void getFoodDonationDataList(FoodDonationDataListCallBack foodDonationDataListCallBack) {
+        getFoodDonationUIDList(foodDonationNotificationUIDList -> {
+            if (!foodDonationNotificationUIDList.isEmpty()) {
+                List<DonateFood> donateFoodDataList = new ArrayList<>();
+                for (String notificationUID : foodDonationNotificationUIDList) {
+                    DatabaseReference foodDonationDataReference = DONATE_FOOD_REFERENCE
+                            .child(notificationUID);
+                    foodDonationDataReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            count++;
+                            DonateFood donateFood = dataSnapshot.getValue(DonateFood.class);
+                            donateFoodDataList.add(donateFood);
+                            if (count == donateFoodDataList.size()) {
+                                foodDonationDataListCallBack.onCallBack(donateFoodDataList);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                        }
+                    });
+                }
+            } else {
+                foodDonationDataListCallBack.onCallBack(new ArrayList<>());
+            }
+        });
+    }
+
+    /* ------------------------------------------------------------- *
+     * Interfaces
+     * ------------------------------------------------------------- */
+
+    interface FoodDonationUIDListCallback {
+        void onCallBack(List<String> foodDonationUIDList);
+    }
+
+    public interface FoodDonationDataListCallBack {
+        void onCallBack(List<DonateFood> donateFoodList);
+    }
+
+}
---
+++ app/src/main/java/com/kirtanlabs/nammaapartments/navigationdrawer/myfood/activities/FoodDonationHistory.java
@@ -0,0 +1,50 @@
+package com.kirtanlabs.nammaapartments.navigationdrawer.myfood.activities;
+
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.kirtanlabs.nammaapartments.BaseActivity;
+import com.kirtanlabs.nammaapartments.R;
+import com.kirtanlabs.nammaapartments.navigationdrawer.myfood.RetrievingFoodDonationList;
+import com.kirtanlabs.nammaapartments.navigationdrawer.myfood.adapters.MyFoodHistoryAdapter;
+
+public class FoodDonationHistory extends BaseActivity {
+
+    /* ------------------------------------------------------------- *
+     * Overriding BaseActivity Objects
+     * ------------------------------------------------------------- */
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_food_donation_history;
+    }
+
+    @Override
+    protected int getActivityTitle() {
+        return R.string.food_donation_history_title;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /*We need Progress Indicator in this screen*/
+        showProgressIndicator();
+
+        /*Getting Id of recycler view*/
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        /*Retrieving Details of all Food Donation requests raised by the user*/
+        new RetrievingFoodDonationList(FoodDonationHistory.this).getFoodDonationDataList(donateFoodList -> {
+            if (donateFoodList.isEmpty()) {
+                hideProgressIndicator();
+                showFeatureUnavailableLayout(R.string.no_food_donations_raised);
+            } else {
+                hideProgressIndicator();
+                recyclerView.setAdapter(new MyFoodHistoryAdapter(FoodDonationHistory.this, donateFoodList));
+            }
+        });
+    }
+}
